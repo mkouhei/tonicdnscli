020c42246240612ca40184053bc5f9d8683cca93
Kouhei Maeda <mkouhei@palmtb.net>
2012-04-14 00:34:26 +0900

    Fix delete mode only
     * Default mode is adding records
     * Add delete mode with option -d/--delete
    
    Signed-off-by: Kouhei Maeda <mkouhei@palmtb.net>

--- json-convert.py
+++ json-convert.py
@@ -17,25 +17,19 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
-class JSONConvert:
-    '''
-    Read text file
-    '''
-    def readFile(self):
-        import os.path, sys
-        self.filename = sys.argv[1]
+from optparse import OptionParser
 
-        if os.path.isfile(self.filename):
-            self.domain = os.path.basename(self.filename).split('.txt')[0]
-            self.f = open(self.filename, 'r')
-
+class JSONConvert():
     '''
     Read record
     '''
-    def readRecords(self):
+    def __init__(self, domain):
+        self.domain = domain
+
+    def readRecords(self,filename):
         import re
         self.records = []
-        for line in self.f:
+        for line in filename:
             # Ignore number(#) at begining of a line.
             if not re.search('^#', line):
                 self.generateDict(line)
@@ -67,17 +61,51 @@ class JSONConvert:
     '''
     Serialize JSON
     '''
-    def serializeJSON(self):
+    def serializeJSON(self, act):
         import json
-        self.build_records = json.JSONEncoder().encode({
-                "name": self.domain,
-                "records": self.records
-                })
+        data = lambda act: {"records": self.records} \
+            if act else {"name": self.domain, "records": self.records}
+        self.build_records = json.JSONEncoder().encode(data(act))
+
+def parse_options():
+    usage = "usage: %prog [options] inputfile"
+    parser = OptionParser(usage=usage)
+    parser.add_option("-d", "--delete", action="store_true",
+                      help="delete records")
+    options, args = parser.parse_args()
+
+    if len(args) == 0:
+        parser.print_help()
+        sys.exit(0)
+
+    return options, args
 
 
-''' Main '''
-o = JSONConvert()
-o.readFile()
-o.readRecords()
-o.serializeJSON()
-print o.build_records
+def main():
+    import os.path, sys
+    try:
+        options, args = parse_options()
+    except RuntimeError, e:
+        sys.stderr.write("ERROR: %s\n" % e)
+        return
+
+    if options.delete:
+        act = False
+    else:
+        act = True
+
+    filename = args[0]
+    print filename
+    if os.path.isfile(filename):
+        d = os.path.basename(filename).split('.txt')[0]
+        f = open(filename, 'r')
+
+    o = JSONConvert(d)
+    o.readRecords(f)
+    o.serializeJSON(act)
+    print o.build_records
+    
+if __name__ == "__main__":
+    main()
+
+
