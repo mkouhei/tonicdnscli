1bf4f1d58bed01ee7a2ead6ae296aa1e1bd64022
Kouhei Maeda <mkouhei@palmtb.net>
2012-04-26 20:27:50 +0900

    Update unittest converter
    Signed-off-by: Kouhei Maeda <mkouhei@palmtb.net>

--- src/tonicdnscli/tests/test_converter.py
+++ src/tonicdnscli/tests/test_converter.py
@@ -9,8 +9,8 @@ from tonicdnscli.converter import JSONConvert
 
 
 class JSONConvertTests(unittest.TestCase):
-
-    l = """# name type content ttl priority
+    def setUp(self):
+        self.str1 = """# name type content ttl priority
 test0.example.org A 10.10.10.10 86400
 test1.example.org A 10.10.10.11 86400
 test2.example.org A 10.10.10.12 86400
@@ -18,8 +18,7 @@ example.org MX mx.example.org 86400 0
 example.org MX mx2.example.org 86400 10
 mx.example.org A 10.10.11.10 3600
 mx2.example.org A\t\t10.10.11.10 3600"""
-
-    lists = ["""test0.example.org A 10.10.10.10 86400
+        self.list1 = ["""test0.example.org A 10.10.10.10 86400
 test1.example.org A 10.10.10.11 86400
 test2.example.org A 10.10.10.12 86400
 example.org MX mx.example.org 86400 0
@@ -27,8 +26,7 @@ example.org MX mx2.example.org 86400 10
 mx.example.org A 10.10.11.10 3600
 mx2.example.org A\t\t10.10.11.10 3600
 """]
-
-    lists2 = ["""test0.example.org A 10.10.10.10 86400
+        self.list2 = ["""test0.example.org A 10.10.10.10 86400
 test1.example.org A 10.10.10.11 86400
 test2.example.org A 10.10.10.12 86400\n""",
 """example.org MX mx.example.org 86400 0
@@ -36,23 +34,29 @@ example.org MX mx2.example.org 86400 10
 mx.example.org A 10.10.11.10 3600\n""",
 'mx2.example.org A\t\t10.10.11.10 3600\n']
 
-    d = [{'content': '10.10.10.10', 'name': 'test0.example.org',
-          'ttl': '86400', 'type': 'A'},
-         {'content': '10.10.10.11', 'name': 'test1.example.org',
-          'ttl': '86400', 'type': 'A'},
-         {'content': '10.10.10.12', 'name': 'test2.example.org',
-          'ttl': '86400', 'type': 'A'},
-         {'content': 'mx.example.org', 'name': 'example.org',
-          'priority': '0', 'ttl': '86400', 'type': 'MX'},
-         {'content': 'mx2.example.org', 'name': 'example.org',
-          'priority': '10', 'ttl': '86400', 'type': 'MX'},
-         {'content': '10.10.11.10', 'name': 'mx.example.org',
-          'ttl': '3600', 'type': 'A'},
-         {'content': '10.10.11.10', 'name': 'mx2.example.org',
-          'ttl': '3600', 'type': 'A'}]
-    l1 = "test0.example.org A 10.10.10.10 86400"
-    l2 = "example.org MX mx.example.org 86400 0"
-    l3 = "mx2.example.org A\t\t10.10.11.10 3600"
+        self.list3 = [{'name': 'test0.example.org', 'type': 'A',
+                       'content': '10.10.10.10', 'ttl': '86400'}]
+        self.list4 = [{'name': 'example.org', 'type': 'MX',
+                       'content': 'mx.example.org',
+                       'ttl': '86400', 'priority': '0'}]
+
+        self.dicts1 = [{'content': '10.10.10.10', 'name': 'test0.example.org',
+                        'ttl': '86400', 'type': 'A'},
+                       {'content': '10.10.10.11', 'name': 'test1.example.org',
+                        'ttl': '86400', 'type': 'A'},
+                       {'content': '10.10.10.12', 'name': 'test2.example.org',
+                        'ttl': '86400', 'type': 'A'},
+                       {'content': 'mx.example.org', 'name': 'example.org',
+                        'priority': '0', 'ttl': '86400', 'type': 'MX'},
+                       {'content': 'mx2.example.org', 'name': 'example.org',
+                        'priority': '10', 'ttl': '86400', 'type': 'MX'},
+                       {'content': '10.10.11.10', 'name': 'mx.example.org',
+                        'ttl': '3600', 'type': 'A'},
+                       {'content': '10.10.11.10', 'name': 'mx2.example.org',
+                        'ttl': '3600', 'type': 'A'}]
+        self.line1 = "test0.example.org A 10.10.10.10 86400"
+        self.line2 = "example.org MX mx.example.org 86400 0"
+        self.line3 = "mx2.example.org A\t\t10.10.11.10 3600"
 
     def test__init__(self):
         o = JSONConvert('example.org')
@@ -64,31 +68,27 @@ mx.example.org A 10.10.11.10 3600\n""",
 
     def test_readRecords(self):
         o = JSONConvert('example.org')
-        o.readRecords(self.l.splitlines())
-        self.assertListEqual(self.d, o.records)
+        o.readRecords(self.str1.splitlines())
+        self.assertListEqual(self.dicts1, o.records)
 
     def test_checkkey(self):
         o = JSONConvert('example.org')
-        self.assertEquals("test0.example.org", o.checkkey(self.l1, 0))
-        self.assertEquals("A", o.checkkey(self.l1, 1))
-        self.assertEquals("10.10.10.10", o.checkkey(self.l1, 2))
-        self.assertEquals("86400", o.checkkey(self.l1, 3))
-        self.assertFalse(o.checkkey(self.l1, 4))
-        self.assertEquals("0", o.checkkey(self.l2, 4))
-        self.assertFalse(o.checkkey(self.l2, 5))
-        self.assertEquals("10.10.11.10", o.checkkey(self.l3, 2))
+        self.assertEquals("test0.example.org", o.checkkey(self.line1, 0))
+        self.assertEquals("A", o.checkkey(self.line1, 1))
+        self.assertEquals("10.10.10.10", o.checkkey(self.line1, 2))
+        self.assertEquals("86400", o.checkkey(self.line1, 3))
+        self.assertFalse(o.checkkey(self.line1, 4))
+        self.assertEquals("0", o.checkkey(self.line2, 4))
+        self.assertFalse(o.checkkey(self.line2, 5))
+        self.assertEquals("10.10.11.10", o.checkkey(self.line3, 2))
 
     def test_generateDict(self):
         o = JSONConvert('example.org')
-        o.generateDict(self.l1)
-        self.assertListEqual([{'name': 'test0.example.org', 'type': 'A',
-                            'content': '10.10.10.10', 'ttl': '86400'}],
-                          o.records)
+        o.generateDict(self.line1)
+        self.assertListEqual(self.list3, o.records)
         o2 = JSONConvert('example.org')
-        o2.generateDict(self.l2)
-        self.assertListEqual([{'name': 'example.org', 'type': 'MX',
-                            'content': 'mx.example.org', 'ttl': '86400',
-                            'priority': '0'}], o2.records)
+        o2.generateDict(self.line2)
+        self.assertListEqual(self.list4, o2.records)
 
     def test_genData(self):
         o = JSONConvert('example.org')
@@ -106,12 +106,12 @@ mx.example.org A 10.10.11.10 3600\n""",
         with open(sample, 'r') as f:
             o.separateInputFile(f)
         self.maxDiff = None
-        self.assertListEqual(self.lists, o.separated_list)
+        self.assertListEqual(self.list1, o.separated_list)
         o2 = JSONConvert('example.org')
         o2.delta = 3
         with open(sample, 'r') as f2:
             o2.separateInputFile(f2)
-        self.assertListEqual(self.lists2, o2.separated_list)
+        self.assertListEqual(self.list2, o2.separated_list)
 
 if __name__ == '__main__':
     unittest.main()
